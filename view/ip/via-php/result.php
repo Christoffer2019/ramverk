a5eaeb002a3304f10819842bc740b299f4ca02bf
Christoffer Lymalm <chly19@student.bth.se>
2020-12-13 14:03:51 +0000

    Added result.php for displaying information on IP.

---
+++ view/ip/via-php/result.php
@@ -0,0 +1,14 @@
+<!-- Display link to the country's Wikipedia page. -->
+<a href="" id="wikipedia"></a>
+
+<!-- Display country flag for IP position -->
+<img src="" alt="Flagga" id="flag">
+<p>Flagga från <a href="https://www.whoishostingthis.com">WhoIsHostingThis.com</a></p>
+
+<!-- Use leaflet to display map -->
+<div id="leaflet-map"></div>
+<script
+    src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+    crossorigin="">
+</script>
